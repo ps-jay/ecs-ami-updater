664242835b3da7b5cd79df5348fd83028475f836
Philip Jay <phil@jay.id.au>
2018-05-26 00:43:35 +1000

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ecs-ami-updater
-Checks if the EC2 host running ECS has booted from the most up-to-date AMI, and if not, updates a Cloudformation stack
+Checks if the EC2 hosts running ECS have been booted from the most up-to-date AMI, and if not, updates a Cloudformation stack
+
+## Expectations
+There are a few:
+
+- The container will have IAM role permissions to call:
+    - ecs:ListAttributes
+    - ssm:GetParameter
+    - cloudformation:UpdateStack
+- The Cloudformation stack to be updated will have the AMI ID as a parameter called `ECSAMI`
+- The Cloudformation stack itself, and the ECS cluster will be given as arguments to the container invokation \
+  (e.g. `docker run image stack_name cluster_name`)
---
+++ ecs_ami_updater.py
@@ -0,0 +1,49 @@
+import argparse
+import boto3
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "stack_name",
+    help="The cloudformation stack to update",
+)
+parser.add_argument(
+    "cluster_name",
+    help="The ECS cluster name to investigate",
+)
+parser.add_argument(
+    "--ami-stack-param",
+    help="The name of the cldouformation stack parameter that stores the AMI ID",
+    default="ECSAMI",
+)
+args = parser.parse_args()
+
+# Get the AMI used in the cluster
+client = boto3.client('ecs')
+# XXX: doesn't handle results > 100 in number
+response = client.list_attributes(
+    cluster=args.cluster_name,
+    targetType='container-instance',
+    attributeName='ecs.ami-id',
+)
+# XXX: only considers the first instance as relevant
+current_ami = response['attributes'][0]['value']
+print("DEBUG: current ECS AMI ID is %s" % current_ami)
+
+# Get latest ECS AMI for the region
+client = boto3.client('ssm')
+response = client.get_parameter(Name='/aws/service/ecs/optimized-ami/amazon-linux/recommended/image_id',)
+latest_ami = response['Parameter']['Value']
+print("DEBUG: latest ECS AMI ID is %s" % latest_ami)
+
+if current_ami != latest_ami:
+    print("INFO: newer AMI is available, updating Cloudformation stack")
+    exit(2)
+    client = boto3.client('cloudformation')
+    response = client.update_stack(
+        StackName=args.stack_name,
+        UsePreviousTemplate=True,
+        Parameters=[{
+            'ParameterKey': args.ami_stack_param,
+            'ParameterValue': latest_ami,
+        }],
+    )
